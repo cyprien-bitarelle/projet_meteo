c08fb10ff6c4c9ab052c4f01aaccc488741271bd
Cyprien <bitarelle.cyprien@gmail.com>
2023-03-15 11:57:12 +0100

    Mise à jour Token

--- meteo.js
+++ meteo.js
@@ -1,5 +1,5 @@
 //Clé API OpenWeatherMap. On la place ici dans le cas où on l'utiliserait dans plusieurs fonctions.
-const apikey = "457228ff47f381f9930416ac2f639732";
+const apikey = config.WEATHER;
 
 async function récupererPaysListe() {
   let select = document.getElementById("pays");
@@ -27,8 +27,7 @@ L.tileLayer(
     tileSize: 512,
     zoomOffset: -1,
     //Token leaflet
-    accessToken:
-      "pk.eyJ1IjoiYmliaXJ6IiwiYSI6ImNrdDRkZ3Y5NDEzMXcycHMybzhvaGp6czQifQ.I3whrNWN0xxkQ5VRej0W-A",
+    accessToken: config.LEAFLET,
   }
 ).addTo(mymap);
 
